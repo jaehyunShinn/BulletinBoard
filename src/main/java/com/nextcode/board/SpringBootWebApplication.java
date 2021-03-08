c6862b260f774003b499243a4203eb374c16fe7f
shin1201 <shin1201@next-code.com>
2021-03-08 15:12:55 +0900

    add db config

--- src/main/java/com/nextcode/board/SpringBootWebApplication.java
+++ src/main/java/com/nextcode/board/SpringBootWebApplication.java
@@ -13,27 +13,8 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SpringBootWebApplication.class) ;
     }
-//        SpringApplication.run(SpringBootWebApplication.class, args);
-//        ApplicationScope01 obj1 = new ApplicationScope01();
-//        ApplicationScope02 obj2 = new ApplicationScope02();
-//        HttpServletRequest h1 = new HttpServletRequest();
-//        obj1.doGet();
-//        obj2.doGet();
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootWebApplication.class, args);
 
     }
-//    @com.nextcode.board.RefreshScope
-//    @RestController
-//    class MessageRestController {
-//
-//        @Value("${message:Hello default}")
-//        private String message;
-//
-//        @RequestMapping("/message")
-//        String getMessage() {
-//            return this.message;
-//        }
-//    @GetMapping("/hello")
-//    public String sayHello(@RequestParam())
 }
---
+++ src/main/java/com/nextcode/board/config/DataSourceConfig.java
@@ -0,0 +1,4 @@
+package com.nextcode.board.config;
+
+public class DataSourceConfig implements TransactionManagementConfigurer{
+}
